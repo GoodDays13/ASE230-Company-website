fb560b035d7b0c5dc2ab003ae239434efae467c4
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2025-10-23 15:01:29 -0400

    Fill in products section

--- index.php
+++ index.php
@@ -90,8 +90,7 @@ $team = readCSVFile("data/team.csv");
             <div class="row justify-content-center">
                 <div class="col-lg-8">
                     <div class="text-center">
-                        <span class="badge badge-soft-primary mb-4">Professional Landing</span>
-                        <h1 class="font-weight-semibold mb-4 hero-3-title">Professional, Multipurpose Landing Page</h1>
+                        <h1 class="font-weight-semibold mb-4 hero-3-title"><?= $company_name ?></h1>
                         <p class="mb-5 text-muted subtitle w-75 mx-auto"><?= $mission_statement ?></p>
 
                         <div>
@@ -124,48 +123,36 @@ $team = readCSVFile("data/team.csv");
         <div class="container">
             <div class="row justify-content-center mb-5">
                 <div class="col-lg-7 text-center">
-                    <h2 class="fw-bold">Our Features</h2>
-                    <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem ab illo inventore.</p>
+                    <h2 class="fw-bold">Our Products</h2>
+                    <p class="text-muted"><?= $overview ?></p>
                 </div>
                 <!-- end col -->
             </div>
             <!-- end row -->
-            <div class="row align-items-center mb-5">
-                <div class="col-md-5 order-2 order-md-1 mt-md-0 mt-5">
-                    <h2 class="mb-4">Perfect Solution For Small Businesses</h2>
-                    <p class="text-muted mb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis..</p>
-                    <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
-                </div>
-                <!-- end col -->
-                <div class="col-md-6 ms-md-auto order-1 order-md-2">
-                    <div class="position-relative">
-                        <div class="ms-5 features-img">
-                            <img src="images/features-1.jpg" alt="" class="img-fluid d-block mx-auto rounded shadow" />
-                        </div>
-                        <img src="images/dot-img.png" alt="" class="dot-img-left" />
+            <?php
+            $i = 0;
+            foreach ($products as $product) {
+                $i++;
+            ?>
+                <div class="row align-items-center justify-content-between <?= $i == 0 ? "mb-5" : "section pb-0" ?>">
+                    <div class="col-md-6 order-1 order-md-<?= $i % 2 == 0 ? '2' : '1' ?> 1 mt-md-0 mt-5">
+                        <h2 class="mb-4"><?= $product->name ?></h2>
+                        <p class="text-muted mb-5"><?= $product->description ?></p>
+                        <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
                     </div>
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-            <div class="row align-items-center section pb-0">
-                <div class="col-md-6">
-                    <div class="position-relative mb-md-0 mb-5">
-                        <div class="me-5 features-img">
-                            <img src="images/features-2.jpg" alt="" class="img-fluid d-block mx-auto rounded shadow" />
-                        </div>
-                        <img src="images/dot-img.png" alt="" class="dot-img-right" />
+                    <!-- end col -->
+                    <div class="col-md-5 order-2 order-md-<?= $i % 2 == 0 ? '1' : '2' ?>">
+                        <ul class="list-group">
+                            <?php
+                            foreach ($product->applications as $app => $desc) {
+                                echo "<li class='list-group-item'>" . $app . ": " . $desc . "</li>";
+                            } ?>
+                        </ul>
                     </div>
+                    <!-- end col -->
                 </div>
-                <!-- end col -->
-                <div class="col-md-5 ms-md-auto">
-                    <h2 class="mb-4">Build community & conversion with our suite of social tool</h2>
-                    <p class="text-muted mb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis..</p>
-                    <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
+                <!-- end row -->
+            <?php } ?>
         </div>
         <!-- end container -->
     </section>
