16eb100e4eafc6b080e574a72b715122a6433f00
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2025-10-23 14:07:59 -0400

    Move awards section down

--- index.php
+++ index.php
@@ -119,6 +119,58 @@ $team = readCSVFile("data/team.csv");
     </section>
     <!-- Hero End -->
 
+    <!-- Features start -->
+    <section class="section bg-light" id="features">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+                <div class="col-lg-7 text-center">
+                    <h2 class="fw-bold">Our Features</h2>
+                    <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem ab illo inventore.</p>
+                </div>
+                <!-- end col -->
+            </div>
+            <!-- end row -->
+            <div class="row align-items-center mb-5">
+                <div class="col-md-5 order-2 order-md-1 mt-md-0 mt-5">
+                    <h2 class="mb-4">Perfect Solution For Small Businesses</h2>
+                    <p class="text-muted mb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis..</p>
+                    <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
+                </div>
+                <!-- end col -->
+                <div class="col-md-6 ms-md-auto order-1 order-md-2">
+                    <div class="position-relative">
+                        <div class="ms-5 features-img">
+                            <img src="images/features-1.jpg" alt="" class="img-fluid d-block mx-auto rounded shadow" />
+                        </div>
+                        <img src="images/dot-img.png" alt="" class="dot-img-left" />
+                    </div>
+                </div>
+                <!-- end col -->
+            </div>
+            <!-- end row -->
+            <div class="row align-items-center section pb-0">
+                <div class="col-md-6">
+                    <div class="position-relative mb-md-0 mb-5">
+                        <div class="me-5 features-img">
+                            <img src="images/features-2.jpg" alt="" class="img-fluid d-block mx-auto rounded shadow" />
+                        </div>
+                        <img src="images/dot-img.png" alt="" class="dot-img-right" />
+                    </div>
+                </div>
+                <!-- end col -->
+                <div class="col-md-5 ms-md-auto">
+                    <h2 class="mb-4">Build community & conversion with our suite of social tool</h2>
+                    <p class="text-muted mb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis..</p>
+                    <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
+                </div>
+                <!-- end col -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </section>
+    <!-- Features end -->
+
     <!-- Services start -->
     <section class="section" id="services">
         <div class="container">
@@ -172,58 +224,6 @@ $team = readCSVFile("data/team.csv");
 
     </section>
     <!-- Services end -->
-
-    <!-- Features start -->
-    <section class="section bg-light" id="features">
-        <div class="container">
-            <div class="row justify-content-center mb-5">
-                <div class="col-lg-7 text-center">
-                    <h2 class="fw-bold">Our Features</h2>
-                    <p class="text-muted">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem ab illo inventore.</p>
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-            <div class="row align-items-center mb-5">
-                <div class="col-md-5 order-2 order-md-1 mt-md-0 mt-5">
-                    <h2 class="mb-4">Perfect Solution For Small Businesses</h2>
-                    <p class="text-muted mb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis..</p>
-                    <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
-                </div>
-                <!-- end col -->
-                <div class="col-md-6 ms-md-auto order-1 order-md-2">
-                    <div class="position-relative">
-                        <div class="ms-5 features-img">
-                            <img src="images/features-1.jpg" alt="" class="img-fluid d-block mx-auto rounded shadow" />
-                        </div>
-                        <img src="images/dot-img.png" alt="" class="dot-img-left" />
-                    </div>
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-            <div class="row align-items-center section pb-0">
-                <div class="col-md-6">
-                    <div class="position-relative mb-md-0 mb-5">
-                        <div class="me-5 features-img">
-                            <img src="images/features-2.jpg" alt="" class="img-fluid d-block mx-auto rounded shadow" />
-                        </div>
-                        <img src="images/dot-img.png" alt="" class="dot-img-right" />
-                    </div>
-                </div>
-                <!-- end col -->
-                <div class="col-md-5 ms-md-auto">
-                    <h2 class="mb-4">Build community & conversion with our suite of social tool</h2>
-                    <p class="text-muted mb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis..</p>
-                    <a href="javascript: void(0);" class="btn btn-primary">Find out more <i class="icon-xs ms-2" data-feather="arrow-right"></i></a>
-                </div>
-                <!-- end col -->
-            </div>
-            <!-- end row -->
-        </div>
-        <!-- end container -->
-    </section>
-    <!-- Features end -->
 
     <section class="section bg-gradient-primary">
         <div class="bg-overlay-img" style="background-image: url(images/demos.png);"></div>
